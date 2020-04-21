e55281dcbf39968430315c2d20b2ebf889f55877
aiwatko <oktawia.kata@thoughtworks.com>
2020-04-21 16:20:58 +0100

    #2 [@gazeddyw / @aiwatko] Add form elements. Add background colour.

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
-function App() {
-  return  <h1>Hello BMMRO</h1>
-}
+const App = () => {
+  return (
+    <main style={{background: 'lightblue'}}>
+      <h1>Hello BMMRO</h1>
+      <Formik
+        initialValues={{ date: '', encSeqNo: '', species: '' }}
+        validate={values => {
+          const errors = {};
+          const errorMessage = 'Required'; 
+          if (!values.date) {
+            errors.date = errorMessage;
+          }
+          if (!values.encSeqNo) {
+            errors.encSeqNo = errorMessage;
+          }
+          if (!values.species) {
+            errors.species = errorMessage;
+          }
+          return errors;
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+      <Form>
+        <Field type="text" name="date" />
+        <ErrorMessage name="date" component="span" />
+        <Field type="text" name="encSeqNo" />
+        <ErrorMessage name="encSeqNo" component="span" />
+        <Field type="text" name="species" />
+        <ErrorMessage name="species" component="span" />
+        <button type="submit">
+          Submit
+        </button>
+      </Form>
+    </Formik>
+    </main>);
+};
 
 export default App;
