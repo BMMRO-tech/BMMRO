1d934610238fb92524ff7b4625a8a8c196028384
aiwatko <oktawia.kata@thoughtworks.com>
2020-04-22 16:21:53 +0100

    #2 [@gazeddyw / @aiwatko] Move Habitat Use form to a separate component.

--- app/src/App.js
+++ app/src/App.js
@@ -1,69 +1,14 @@
 /** @jsx jsx */
-import { Formik, Form } from "formik";
-import { css, jsx } from "@emotion/core";
+import { jsx } from "@emotion/core";
 
 import Layout from "./components/Layout";
-import Input from "./components/Input";
-import Button from "./components/Button";
-
-const fields = [
-  { name: "date", label: "Date" },
-  { name: "encSeqNo", label: "Enc Seq #" },
-  { name: "species", label: "Species" },
-];
+import HabitatUseForm from "./components/HabitatUseForm";
 
 const App = () => {
-  const styles = {
-    inputContainer: css`
-      margin-bottom: 15px;
-    `,
-  };
-  
   return (
     <Layout>
       <h1>Hello BMMRO</h1>
-      <Formik
-        initialValues={{
-          [fields[0].name]: "",
-          [fields[1].name]: "",
-          [fields[2].name]: "",
-        }}
-        validate={(values) => {
-          const errors = {};
-          const errorMessage = "Required";
-          if (!values.date) {
-            errors.date = errorMessage;
-          }
-          if (!values.encSeqNo) {
-            errors.encSeqNo = errorMessage;
-          }
-          if (!values.species) {
-            errors.species = errorMessage;
-          }
-          return errors;
-        }}
-        onSubmit={(values) => console.log(values)}
-      >
-        {({ handleChange, handleBlur, touched, values, errors }) => (
-          <Form>
-            {fields.map(({ name, label }) => (
-              <div css={styles.inputContainer}>
-                <Input
-                  type="text"
-                  name={name}
-                  label={label}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  touched={touched[name]}
-                  value={values[name]}
-                  error={errors[name]}
-                />
-              </div>
-            ))}
-            <Button type="submit">Submit</Button>
-          </Form>
-        )}
-      </Formik>
+      <HabitatUseForm />
     </Layout>
   );
 };
---
+++ app/src/components/HabitatUseForm.jsx
@@ -0,0 +1,66 @@
+/** @jsx jsx */
+import { Formik, Form } from "formik";
+import { css, jsx } from "@emotion/core";
+
+import Input from "./Input";
+import Button from "./Button";
+
+const fields = [
+  { name: "date", label: "Date" },
+  { name: "encSeqNo", label: "Enc Seq #" },
+  { name: "species", label: "Species" },
+];
+
+const HabitatUseForm = () => {
+  const styles = {
+    inputContainer: css`
+      margin-bottom: 15px;
+    `,
+  };
+  return (
+    <Formik
+      initialValues={{
+        [fields[0].name]: "",
+        [fields[1].name]: "",
+        [fields[2].name]: "",
+      }}
+      validate={(values) => {
+        const errors = {};
+        const errorMessage = "Required";
+        if (!values.date) {
+          errors.date = errorMessage;
+        }
+        if (!values.encSeqNo) {
+          errors.encSeqNo = errorMessage;
+        }
+        if (!values.species) {
+          errors.species = errorMessage;
+        }
+        return errors;
+      }}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ handleChange, handleBlur, touched, values, errors }) => (
+        <Form>
+          {fields.map(({ name, label }) => (
+            <div css={styles.inputContainer}>
+              <Input
+                type="text"
+                name={name}
+                label={label}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                touched={touched[name]}
+                value={values[name]}
+                error={errors[name]}
+              />
+            </div>
+          ))}
+          <Button type="submit">Submit</Button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default HabitatUseForm;
