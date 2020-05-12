e3aaaf422642d2734d47e9896e011f86e626c6f5
dsobkow <dorota.sobkow@yahoo.co.uk>
2020-05-12 16:31:14 +0100

    [#5] Test listenForPendingHabitatUseRecords method.
    [@dsobkow / @aiwatko]

--- app/src/datastore/__tests__/datastore.test.js
+++ app/src/datastore/__tests__/datastore.test.js
@@ -1,35 +1,99 @@
 import { Datastore } from "../datastore";
 
 describe("Datastore with firestore", () => {
-  it("should add expected data to habitat use collection", async () => {
+  const buildDatastoreMock = (mock) => {
     const firestoreMock = {
-      collection: jest.fn().mockReturnValue({
-        add: jest.fn().mockResolvedValue({ id: "abc123" }),
-      }),
+      collection: jest.fn().mockReturnValue(mock),
     };
 
-    const datastore = new Datastore(firestoreMock);
+    return new Datastore(firestoreMock);
+  };
 
-    const result = await datastore.createHabitatUse({ value1: "123" });
+  describe("createHabitatUse", () => {
+    it("should add expected data to habitat use collection", async () => {
+      const collectionReturnMock = {
+        add: jest.fn().mockResolvedValue({ id: "abc123" }),
+      };
+      const datastore = buildDatastoreMock(collectionReturnMock);
 
-    expect(firestoreMock.collection).toHaveBeenCalledTimes(1);
-    expect(firestoreMock.collection().add).toHaveBeenCalledWith({
-      value1: "123",
+      const actual = await datastore.createHabitatUse({ value1: "123" });
+      expect(actual).toEqual("abc123");
     });
-    expect(result).toEqual("abc123");
+
+    it("should throw exception", async () => {
+      const collectionReturnMock = {
+        add: jest.fn().mockRejectedValue(new Error("mango")),
+      };
+      const datastore = buildDatastoreMock(collectionReturnMock);
+
+      await expect(datastore.createHabitatUse("123")).rejects.toThrow(
+        new Error("in createHabitatUse: mango")
+      );
+    });
   });
 
-  it("should throw exception", async () => {
-    const firestoreMock = {
-      collection: jest.fn().mockReturnValue({
-        add: jest.fn().mockRejectedValue(new Error("mango")),
-      }),
-    };
+  describe("listenForPendingHabitatUseRecords", () => {
+    it("should perform an action defined in the callback on pending records", () => {
+      const collectionReturnMock = {
+        onSnapshot: jest.fn(({}, success) =>
+          success({
+            docs: [
+              {
+                metadata: {
+                  hasPendingWrites: true,
+                },
+                data: jest.fn(() => ({
+                  date: "20/02/2020",
+                  time: "15:30",
+                })),
+              },
+            ],
+          })
+        ),
+      };
 
-    const datastore = new Datastore(firestoreMock);
+      const datastore = buildDatastoreMock(collectionReturnMock);
+      let actualRecords;
+      const saveRecords = (data) => {
+        actualRecords = data;
+      };
+      datastore.listenForPendingHabitatUseRecords(saveRecords);
 
-    await expect(datastore.createHabitatUse("123")).rejects.toThrow(
-      new Error("in createHabitatUse: mango")
-    );
+      const expectedRecords = [
+        {
+          date: "20/02/2020",
+          time: "15:30",
+        },
+      ];
+
+      expect(actualRecords).toStrictEqual(expectedRecords);
+    });
+
+    it("should ignore not pending records", () => {
+      const collectionReturnMock = {
+        onSnapshot: jest.fn(({}, success) =>
+          success({
+            docs: [
+              {
+                metadata: {
+                  hasPendingWrites: false,
+                },
+              },
+            ],
+          })
+        ),
+      };
+
+      const datastore = buildDatastoreMock(collectionReturnMock);
+      let actualRecords;
+      const saveRecords = (data) => {
+        actualRecords = data;
+      };
+      datastore.listenForPendingHabitatUseRecords(saveRecords);
+
+      const expectedRecords = [];
+
+      expect(actualRecords).toStrictEqual(expectedRecords);
+    });
   });
 });
