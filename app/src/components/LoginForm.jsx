dc93cbfa5a13fc5eeb72775a9c47ec9cf9b36d34
Flávia Falé <flavia.rodriguesf@gmail.com>
2020-06-17 16:05:05 +0100

    [#19] Refactor LoginForm component to use Formik
    [@dsobkow / @rahul-rakshit / @flaviarodriguesf]

--- app/src/components/LoginForm.jsx
+++ app/src/components/LoginForm.jsx
@@ -1,23 +1,21 @@
 /** @jsx jsx */
 import { useState, useContext, Fragment } from "react";
+import { Formik, Form } from "formik";
 import { jsx } from "@emotion/core";
 import { FirebaseContext } from "../firebaseContext/firebaseContext";
 import firebaseApp from "../firebaseContext/firebase";
 import { useLoginRedirect } from "../hooks/useLoginRedirect";
 import { AuthenticationErrorType } from "../constants/authentication";
 import ErrorMessage from "./ErrorMessage";
+import Button from "./Button";
+import Input from "./Input";
 
 const LoginForm = () => {
   const { loggedInUser } = useContext(FirebaseContext);
   useLoginRedirect(loggedInUser);
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [loginError, setLoginError] = useState(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
+  const handleSubmit = ({ email, password }) => {
     firebaseApp
       .auth()
       .signInWithEmailAndPassword(email, password)
@@ -29,29 +27,32 @@ const LoginForm = () => {
   return (
     <Fragment>
       <h1 data-testid="login-page">Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          data-testid="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          placeholder="email"
-        />
-        <input
-          data-testid="password"
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          placeholder="password"
-        />
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <Input
+            config={{
+              label: "Email Address",
+              type: "email",
+              name: "email",
+            }}
+          />
+          <Input
+            config={{
+              label: "Password",
+              type: "password",
+              name: "password",
+            }}
+          />
+          <Button testId="submit" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Formik>
 
-        <button data-testid="submit" type="submit">
-          Sign in
-        </button>
-
-        {!!loginError && (
-          <ErrorMessage error={loginError} testId="login-error" />
-        )}
-      </form>
+      {!!loginError && <ErrorMessage error={loginError} testId="login-error" />}
     </Fragment>
   );
 };
