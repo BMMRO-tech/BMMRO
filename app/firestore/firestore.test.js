d49b0e5abac17705e680b0cb98fede544aa88cd1
aiwatko <oktawia.kata@thoughtworks.com>
2020-07-20 11:42:08 +0100

    [#73] Test security rules for encounter collection with habitat use subcollection.
    [@aiwatko / @peersmg / @dsobkow]

--- app/firestore/firestore.test.js
+++ app/firestore/firestore.test.js
@@ -38,3 +38,76 @@ describe("Habitat Use Collection", () => {
     );
   });
 });
+
+describe("Encounter collection with habitat use subcollection", () => {
+  let db;
+  const collectionName = "encounter";
+  const subcollectionName = "habitatUseTest";
+
+  const initializeFirestore = (auth) => {
+    return firebase.initializeTestApp({ projectId, auth }).firestore();
+  };
+
+  afterEach(async () => {
+    await firebase.clearFirestoreData({ projectId });
+    await Promise.all(firebase.apps().map((app) => app.delete()));
+  });
+
+  beforeEach(async () => {
+    await firebase.loadFirestoreRules({ projectId, rules });
+  });
+
+  const defaultValues = {
+    numberField: 1,
+    textField: "Tight",
+  };
+
+  describe("Encounter collection", () => {
+    it("should successfully submit data when user is authenticated", async () => {
+      db = initializeFirestore({ uid: "testId" });
+
+      await firebase.assertSucceeds(
+        db.collection(collectionName).add(defaultValues)
+      );
+    });
+
+    it("should fail to submit data when user is not authenticated", async () => {
+      db = initializeFirestore(null);
+
+      await firebase.assertFails(
+        db.collection(collectionName).add(defaultValues)
+      );
+    });
+  });
+
+  describe("Habitat use subcollection", () => {
+    it("should successfully submit data when user is authenticated", async () => {
+      db = initializeFirestore({ uid: "testId" });
+      const { id } = await db.collection(collectionName).add(defaultValues);
+
+      await firebase.assertSucceeds(
+        db
+          .collection(collectionName)
+          .doc(id)
+          .collection(subcollectionName)
+          .add(defaultValues)
+      );
+    });
+
+    it("should fail to submit data when user is not authenticated", async () => {
+      db = initializeFirestore({ uid: "testId" });
+      const { id } = await db.collection(collectionName).add(defaultValues);
+
+      // Firebase emulator doesn't provide a way to sign the user out
+      db = initializeFirestore(null);
+
+      await firebase.assertFails(
+        db
+          .collection(collectionName)
+          .doc(id)
+          .collection(subcollectionName)
+          .add(defaultValues)
+      );
+    });
+  });
+});
