bd11a51879fc26dd9a0270e7dcc9db6cbb89290f
peersmg <peersmg@gmail.com>
2020-07-23 17:01:32 +0100

    [#72] Add submit button to encounter form.
    For now this button simply prints the values to console and redirects to encounter overview.
    
    [@peersmg]

--- app/src/components/EncounterForm.jsx
+++ app/src/components/EncounterForm.jsx
@@ -1,11 +1,18 @@
 /** @jsx jsx */
 import { Formik, Form } from "formik";
+import { navigate } from "@reach/router";
 import { css, jsx } from "@emotion/core";
+
 import TextInput from "./formFields/TextInput/TextInput";
 import DateInput from "./formFields/DateInput/DateInput";
 import Select from "./formFields/Select/Select";
+import Button from "./Button";
+
 import areaOptions from "../constants/areaOptions";
 import speciesOptions from "../constants/speciesOptions";
+import { ROUTES } from "../constants/routes";
+
+import clientPersistence from "../clientPersistence/clientPersistence";
 
 const styles = {
   container: css`
@@ -39,6 +46,11 @@ const EncounterForm = () => {
           area: "",
           species: "",
         }}
+        onSubmit={(values) => {
+          console.log(values);
+          clientPersistence.set("openEncounterId", "abc");
+          navigate(ROUTES.openEncounter);
+        }}
       >
         <Form>
           <div css={styles.formContainer}>
@@ -61,6 +73,7 @@ const EncounterForm = () => {
               isRequired={true}
             />
           </div>
+          <Button type="submit">Submit</Button>
         </Form>
       </Formik>
     </div>
