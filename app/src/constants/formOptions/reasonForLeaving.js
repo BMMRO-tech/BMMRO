23b093adeb5f894a0680f6be4ca57b630fd367e3
Rahul Rakshit <rahul.rakshit@posteo.net>
2020-08-11 18:04:58 +0200

    [#74] Change a reason for leaving option.
    [@dsobkow / @flaviarodriguesf / @rahul-rakshit / @jhughes-tw ]

--- app/src/constants/formOptions/reasonForLeaving.js
+++ app/src/constants/formOptions/reasonForLeaving.js
@@ -1,6 +1,6 @@
 export default [
   "Continuing search - all animals ID'd",
-  "Continuing search - low priority, higher priority species seen",
+  "Continuing search - higher priority species seen",
   "Continuing search - some samples, some ID'd",
   "Group lost",
   "Group not approachable",
