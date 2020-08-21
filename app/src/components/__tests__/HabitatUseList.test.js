b9d0b378d8152446a9318e74cc6be39d7da22faa
peersmg <peersmg@gmail.com>
2020-08-21 11:17:24 +0100

    [#47] Add test for no habitat entries.
    [@peersmg]

--- app/src/components/__tests__/HabitatUseList.test.js
+++ app/src/components/__tests__/HabitatUseList.test.js
@@ -5,16 +5,24 @@ import HabitatUseList from "../HabitatUseList";
 describe("HabitatUseList", () => {
   it("displays habitat use forms sorted by start time", () => {
     const items = [
-      { data: { startTime: "11:00" }, id: "123" },
-      { data: { startTime: "11:10" }, id: "456" },
-      { data: { startTime: "11:20" }, id: "789" },
+      { data: { startTime: "11:00:10" }, id: "123" },
+      { data: { startTime: "11:00:20" }, id: "456" },
+      { data: { startTime: "11:20:33" }, id: "789" },
     ];
     const { queryAllByRole } = render(<HabitatUseList items={items} />);
 
     const actual = queryAllByRole("listitem");
 
-    expect(actual[0]).toHaveTextContent("11:20");
-    expect(actual[1]).toHaveTextContent("11:10");
-    expect(actual[2]).toHaveTextContent("11:00");
+    expect(actual[0]).toHaveTextContent("11:20:33");
+    expect(actual[1]).toHaveTextContent("11:00:20");
+    expect(actual[2]).toHaveTextContent("11:00:10");
+  });
+
+  it("displays message when there are no list items", () => {
+    const { queryByText } = render(<HabitatUseList items={[]} />);
+
+    const noEntriesMessage = queryByText("No habitat use entries yet");
+
+    expect(noEntriesMessage).not.toBeNull();
   });
 });
