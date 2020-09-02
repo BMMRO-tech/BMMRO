c27d180eb557c6db89193697148c15b9636d4306
peersmg <peersmg@gmail.com>
2020-09-02 16:12:45 +0100

    Only focus on errors after validating

--- app/src/components/formFields/InputFocusOnError.jsx
+++ app/src/components/formFields/InputFocusOnError.jsx
@@ -6,23 +6,24 @@ const isDatepicker = (errorField) => {
 };
 
 const InputFocusOnError = () => {
-  const { errors, isSubmitting, validateForm } = useFormikContext();
+  const { isSubmitting, validateForm } = useFormikContext();
   useEffect(() => {
-    validateForm();
-    if (isSubmitting) {
-      const errorField = Object.keys(errors)[0];
-      console.log(errors);
+    validateForm().then((errors) => {
+      if (isSubmitting) {
+        const errorField = Object.keys(errors)[0];
 
-      if (errorField) {
-        // ReactDOM.findDOMNode is deprecated in Strict Mode
-        const firstFieldWithError = document.getElementsByName(errorField)[0];
-        if (isDatepicker(firstFieldWithError)) {
-          firstFieldWithError.scrollIntoView({ block: "center" });
-        } else {
-          firstFieldWithError.focus();
+        if (errorField) {
+          // ReactDOM.findDOMNode is deprecated in Strict Mode
+          const firstFieldWithError = document.getElementsByName(errorField)[0];
+          if (isDatepicker(firstFieldWithError)) {
+            firstFieldWithError.scrollIntoView({ block: "center" });
+          } else {
+            firstFieldWithError.focus();
+          }
         }
       }
-    }
+    });
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isSubmitting]);
   return null;
