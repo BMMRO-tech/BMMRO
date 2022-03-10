38f7608c8f3880c8ef0f9455e32a1dc722f3b02d
Nenyi Arkaah <nenyi.arkaah@opengi.co.uk>
2022-03-10 17:02:22 +0000

    [#247] creation of a new workflow to handle Pull Requests

--- e2e-tests/index.test.js
+++ e2e-tests/index.test.js
@@ -14,54 +14,56 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function startDriver(){
+async function startDriver() {
   let driver = await new wd.Builder().forBrowser('safari').build();
   return driver;
 }
 
-describe('create a new encounter user journey',()=>{
+describe('create a new encounter user journey', () => {
 
   let driver;
   let encounterId;
   let habitatId;
+  let pageTimeout = 5000;
+  let testTimeout = 20000;
 
-  beforeAll(async() => {
+  beforeAll(async () => {
     driver = await startDriver();
-  },20000)
+  }, testTimeout)
 
-  it('user successfully logs in', async()=>{
+  it('user successfully logs in', async () => {
 
     await driver.get(process.env.ENDPOINT);
 
-    await driver.manage().setTimeouts( { implicit: 10000 } );
+    await driver.manage().setTimeouts({ implicit: pageTimeout });
 
     let email = driver.findElement(wd.By.name('email'));
     let password = driver.findElement(wd.By.name('password'));
 
     await email.sendKeys(process.env.EMAIL);
     await password.sendKeys(process.env.PASSWORD);
-    
+
     await driver.findElement(wd.By.css('button')).click();
 
-    await driver.wait(wd.until.elementLocated(wd.By.css('.css-iwuz5a-Encounters')),10000);
-    
+    await driver.wait(wd.until.elementLocated(wd.By.css('.css-iwuz5a-Encounters')), pageTimeout);
+
     let homeUrl = await driver.getCurrentUrl();
 
     expect(homeUrl).toBe(`${process.env.ENDPOINT}/encounters`);
-  })
+  }, testTimeout)
 
-  it('user creates a new encounter', async()=>{
+  it('user creates a new encounter', async () => {
 
     await driver.findElement(wd.By.css('#newEncounter')).click();
 
-    await driver.manage().setTimeouts( { implicit: 10000 } );
+    await driver.manage().setTimeouts({ implicit: pageTimeout });
 
-    let newEncouterUrl = await driver.getCurrentUrl();
+    let newEncounterUrl = await driver.getCurrentUrl();
 
-    expect(newEncouterUrl).toBe(`${process.env.ENDPOINT}/encounters/new`);
-  })
+    expect(newEncounterUrl).toBe(`${process.env.ENDPOINT}/encounters/new`);
+  }, testTimeout)
 
-  it('user creates a new habitat', async()=>{
+  it('user creates a new habitat', async () => {
 
     let seqNum = await driver.findElement(wd.By.name('sequenceNumber'));
 
@@ -70,81 +72,80 @@ describe('create a new encounter user journey',()=>{
 
     await driver.findElement(wd.By.css('#newHabitat')).click();
 
-    await driver.manage().setTimeouts( { implicit: 10000 } );
+    await driver.manage().setTimeouts({ implicit: pageTimeout });
 
     let newHabitatUrl = await driver.getCurrentUrl();
 
     await driver.findElement(wd.By.css('#saveHabitat')).click();
 
     expect(newHabitatUrl).toContain("/habitat-uses/new");
-  })
+  }, testTimeout)
 
-  it('stores encouter and habitat ID', async()=>{
+  it('stores encounter and habitat ID', async () => {
 
-    await driver.manage().setTimeouts( { implicit: 10000 } );
+    await driver.manage().setTimeouts({ implicit: pageTimeout });
 
     let newHabitatUrl = await driver.findElement(wd.By.css('#habitatUse')).getAttribute("href");
 
     encounterId = newHabitatUrl.split('/')[4];
     habitatId = newHabitatUrl.split('/')[6];
 
-  })
+  }, testTimeout)
 
-  it('user edits encounter', async()=>{
+  it('user edits encounter', async () => {
 
     await driver.findElement(wd.By.css('#encounterDataSheet')).click();
 
-    await driver.manage().setTimeouts( { implicit: 10000 } );
+    await driver.manage().setTimeouts({ implicit: pageTimeout });
 
     let editEncouterUrl = await driver.getCurrentUrl();
 
     expect(editEncouterUrl).toContain("/edit");
-  })
+  }, testTimeout)
 
-  it('user ends encounter', async()=>{
+  it('user ends encounter', async () => {
 
     await driver.findElement(wd.By.css('#species>option[value="Atlantic spotted dolphin"]')).click();
 
     await driver.findElement(wd.By.css('#saveEndEncounter')).click();
 
-    await driver.wait(wd.until.elementLocated(wd.By.css('.css-iwuz5a-Encounters')),10000);
+    await driver.wait(wd.until.elementLocated(wd.By.css('.css-iwuz5a-Encounters')), 10000);
 
     let homeUrl = await driver.getCurrentUrl();
 
     expect(homeUrl).toBe(`${process.env.ENDPOINT}/encounters`);
-  })
+  }, testTimeout)
 
-  it('checks database for new encounter', async()=>{
+  it('checks database for new encounter', async () => {
 
     const docRefEncounter = doc(db, "encounter", encounterId);
     const docSnapEncounter = await getDoc(docRefEncounter);
 
     expect(docSnapEncounter.exists()).toBeTruthy()
-  })
+  }, testTimeout)
 
+  it('checks database for new habitat', async () => {
 
-  it('checks database for new habitat', async()=>{
-
-    const docRefHabitat = doc(db,"encounter", encounterId, "habitatUse", habitatId);
+    const docRefHabitat = doc(db, "encounter", encounterId, "habitatUse", habitatId);
     const docSnapHabitat = await getDoc(docRefHabitat);
 
     expect(docSnapHabitat.exists()).toBeTruthy()
-  })
-  
-  it('deletes habitat and encounter from database', async() => {
+  }, testTimeout)
 
-      await deleteDoc(doc(db, "encounter", encounterId, "habitatUse", habitatId));
-      await deleteDoc(doc(db, "encounter", encounterId));
+  it('deletes habitat and encounter from database', async () => {
 
-      const docRefEncounter = doc(db, "encounter", encounterId);
+    await deleteDoc(doc(db, "encounter", encounterId, "habitatUse", habitatId));
+    await deleteDoc(doc(db, "encounter", encounterId));
 
-      const deletedEncounter = await getDoc(docRefEncounter);
+    const docRefEncounter = doc(db, "encounter", encounterId);
 
-      expect(deletedEncounter.exists()).toBeFalsy()
-  })
+    const deletedEncounter = await getDoc(docRefEncounter);
 
-  afterAll(async() => {
+    expect(deletedEncounter.exists()).toBeFalsy()
+  }, testTimeout)
+
+  afterAll(async () => {
     await driver.quit();
-  })
+  }, testTimeout)
 
 })
