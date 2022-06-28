bae904a4fd03172d854e3f4b532a94230e9cfa3a
Fallon RIShiva <fallon@Fallons-MacBook-Pro.local>
2022-06-28 12:06:35 +0100

    [#293] YML edit

---
+++ app/src/components/BiopsyForm.jsx
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import Button from "../components/Button";
+import { Link } from "@reach/router";
+import { generateOpenEncounterURL } from "../constants/routes";
+
+
+const BiopsyForm = ({ encounterId }) => {
+    return
+        <div>
+            
+                <Link to={generateOpenEncounterURL(encounterId)}>
+                    <Button testId={"cancelBiopsy"}> Cancel </Button>
+                </Link>
+        </div>
+}
