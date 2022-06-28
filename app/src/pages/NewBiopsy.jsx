35c9ee5cad8882e9e2c6a35b4c39f1d7b99dc088
Fallon RIShiva <fallon@Fallons-MacBook-Pro.local>
2022-06-28 16:17:34 +0100

    [#293] JG/FR changed page NewBiopsyForm name to NewBiopsy to avoid confusion

---
+++ app/src/pages/NewBiopsy.jsx
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import Layout from "../components/Layout";
+import utilities from "../materials/utilities";
+import BiopsyForm from "../components/BiopsyForm";
+
+const NewBiopsy = ({ encounterId }) => {
+  return (
+    <Layout hasDefaultPadding={false}>
+      <h1 css={utilities.form.title}>Add New Biopsy Record</h1>
+        <BiopsyForm encounterId={encounterId}/>
+    </Layout>
+  );
+};
+
+export default NewBiopsy;
