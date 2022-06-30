4cf55340cc81c2c46a53047c6fdd9de90a97ccdd
Fallon RIShiva <fallon@Fallons-MacBook-Pro.local>
2022-06-30 14:27:08 +0100

    [#305] FR/ZG added species dropdown to biopsy form

--- app/src/components/BiopsyForm.jsx
+++ app/src/components/BiopsyForm.jsx
@@ -1,10 +1,18 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
-import Button from "../components/Button";
 import { Link } from "@reach/router";
-import { generateOpenEncounterURL } from "../constants/routes";
+import { Formik, Form } from "formik";
+import { useState } from "react";
 
 import utilities from "../materials/utilities";
+import Button from "./Button";
+import FormSection from "./FormSection";
+import ListHeader from "./list/ListHeader";
+
+import Select from "./formFields/Select/Select";
+
+import species from "../constants/formOptions/species";
+import { generateOpenEncounterURL } from "../constants/routes";
 
 const styles = {
   cancelButton: css`
@@ -15,21 +23,47 @@ const styles = {
   `,
 };
 
-const BiopsyForm = ({ encounterId }) => {
+const BiopsyForm = ({ encounterId, handleSubmit, isViewOnly }) => {
+  const [submitType, setSubmitType] = useState(null);
   return (
     <div css={utilities.sticky.contentContainer}>
       <div css={utilities.form.container}>
-        <div css={utilities.sticky.footerContainer}>
-          <Link to={generateOpenEncounterURL(encounterId)}>
-            <Button
-              styles={styles.cancelButton}
-              variant="secondary"
-              testId={"cancelBiopsy"}
-            >
-              Cancel
-            </Button>
-          </Link>
-        </div>
+        <Formik
+          onSubmit={(values) => {
+            handleSubmit(submitType, values);
+          }}
+        >
+          {({ values, submitForm }) => (
+            <Form>
+              <section css={styles.section}>
+                <ListHeader title="Encounter details" />
+                <FormSection>
+                  <Select
+                    name="species"
+                    labelText="Species"
+                    options={species}
+                    isRequired
+                    isDisabled={isViewOnly}
+                  />
+                </FormSection>
+              </section>
+              <div css={utilities.sticky.footerContainer}>
+                <Link to={generateOpenEncounterURL(encounterId)}>
+                  <Button
+                    styles={styles.cancelButton}
+                    variant="secondary"
+                    testId={"cancelBiopsy"}
+                  >
+                    Cancel
+                  </Button>
+                </Link>
+                <Button type="submit" testId={"saveBiopsy"}>
+                    Save
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
--- app/src/components/__tests__/BiopsyForm.test.js
+++ app/src/components/__tests__/BiopsyForm.test.js
@@ -1,8 +1,79 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 import BiopsyForm from "../BiopsyForm";
 
 describe("BiopsyForm", () => {
+  beforeAll(() => {
+    global.Date.now = jest.fn(() =>
+      new Date("2020-05-04T11:30:12.000Z").getTime()
+    );
+  });
+
+  it("submits the form with correct values if all fields are completed", async () => {
+    const realGeolocation = global.navigator.geolocation;
+    const mockAttempt = "1";
+    const mockMark = "123";
+    const samplerName = "T.U.";
+
+    global.navigator.geolocation = {
+      getCurrentPosition: jest.fn().mockImplementation((success) =>
+        success({
+          coords: {
+            latitude: 1.123456,
+            longitude: 1.123456,
+          },
+        })
+      ),
+    };
+
+    let formValues;
+    const mockHandleSubmit = (values) => {
+      formValues = values;
+    };
+    const { getByRole } = render(
+      <BiopsyForm handleSubmit={mockHandleSubmit} />
+    );
+
+    await act(async () => {
+      const speciesInput = getByRole("combobox", { name: "Species *" });
+      const latitudeInput = getByRole("spinbutton", { name: "Lat" });
+      const longitudeInput = getByRole("spinbutton", { name: "Long" });
+      const samplerNameInput = getByRole("textbox", { name: "Sampler Name" });
+      const attemptInput = getByRole("textbox", { name: "Attempt" });
+      const markInput = getByRole("textbox", { name: "Mark" });
+      const totalSpecimensInput = getByRole("textbox", {
+        name: "Total specimens",
+      });
+
+      const submitButton = getByRole("button", { name: "Save" });
+
+      userEvent.selectOptions(speciesInput, "Fin whale");
+      userEvent.clear(latitudeInput);
+      await userEvent.type(latitudeInput, "15.123456");
+      userEvent.clear(longitudeInput);
+      await userEvent.type(longitudeInput, "10.101010");
+      await userEvent.type(attemptInput, "1");
+      await userEvent.type(samplerNameInput, "T.U.");
+      await userEvent.type(markInput, "123");
+      await userEvent.type(totalSpecimensInput, "5");
+      userEvent.click(submitButton);
+    });
+
+    expect(formValues.species).toEqual("Fin whale");
+    expect(formValues.latitude).toEqual("15.123456");
+    expect(formValues.longitude).toEqual("10.101010");
+    expect(formValues.mark).toEqual("123");
+    expect(formValues.startTime).toEqual("11:30:12");
+    expect(formValues.longitude).toEqual("10.101010");
+    expect(formValues.startTime).toEqual("11:30:12");
+    expect(formValues.Date).toEqual("2020-05-04");
+    expect(formValues.endTime).toEqual("");
+    expect(formValues.attempt).toEqual("1");
+    expect(formValues.samplerName).toEqual("T.U.");
+
+    global.navigator.geolocation = realGeolocation;
+  });
+
   it("contains cancel button", () => {
     const { queryByRole } = render(<BiopsyForm />);
 
