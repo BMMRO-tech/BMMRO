f24b71a6d3f5e5925630e6d7efd2e6b968e89cdf
Fallon RIShiva <fallon@pc-2-220.customer.ask4.lan>
2022-07-01 15:10:41 +0100

    [#305] ZG/FR removed non-functional code and tests from biopsy form

--- app/src/components/BiopsyForm.jsx
+++ app/src/components/BiopsyForm.jsx
@@ -2,42 +2,23 @@
 import { css, jsx } from "@emotion/core";
 import { Link } from "@reach/router";
 import { Formik, Form } from "formik";
-import { useState } from "react";
 
 import utilities from "../materials/utilities";
 import Button from "./Button";
 import FormSection from "./FormSection";
 import ListHeader from "./list/ListHeader";
 import biopsyFormDefaults from "../constants/biopsyFormDefaultValues";
-import DateInput from "./formFields/DateInput/DateInput";
-import TimeInput from "./formFields/TimeInput/TimeInput";
-import TextInput from "./formFields/TextInput/TextInput";
-import PositionInput from "./formFields/PositionInput/PositionInput";
+
 
 import Select from "./formFields/Select/Select";
-import { Refresh } from "./icons/Refresh";
 
 import species from "../constants/formOptions/species";
 import { generateOpenEncounterURL } from "../constants/routes";
 
 const BiopsyForm = ({
-  initialValues,
   handleSubmit,
-  isViewOnly,
   encounterId,
-  isPageNewBiopsyForm,
 }) => {
-  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
-  const [showPositionalModal, setShowPositionalModal] = useState({
-    boolean: false,
-    values: "",
-  });
-  const [closedPositionalModal, setClosedPositionalModal] = useState(false);
-  const [refreshLatLong, setRefreshLatLong] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const [position, setPosition] = useState({ latitude: "", longitude: "" });
-  const [error, setError] = useState(null);
-
   const styles = {
     cancelButton: css`
       margin-right: 10px;
@@ -46,20 +27,13 @@ const BiopsyForm = ({
       background-color: none;
     `,
   };
-  const initValues = initialValues || biopsyFormDefaults;
-
+  
   return (
     <div css={utilities.sticky.contentContainer}>
       <div css={utilities.form.container}>
         <Formik
-          initialValues={initValues}
-          async
           onSubmit={(values) => {
-            values.hasEnded ||
-            (values.longitude && values.latitude) ||
-            values.gpsMark
-              ? handleSubmit(values)
-              : setShowPositionalModal({ boolean: true, values: values });
+            handleSubmit(values);
           }}
         >
           {({ values }) => (
@@ -72,84 +46,10 @@ const BiopsyForm = ({
                     labelText="Species"
                     options={species}
                     isRequired
-                    isDisabled={isViewOnly}
                   />
                 </FormSection>
-                {/* <br /> */}
-                <FormSection>
-                  <DateInput
-                    name="startTimestamp"
-                    labelText="Date"
-                    isRequired
-                    isShort
-                    notAfter={new Date()}
-                    autofill={true}
-                  />
-                  <TimeInput
-                    name="startTime"
-                    labelText="Time"
-                    isShort
-                    autofill={true}
-                    notAfter={values.startTimestamp}
-                    isRequired
-                  />
-                </FormSection>
-                <FormSection>
-                  <TextInput
-                    name="attemptNumber"
-                    labelText="Attempt #"
-                    maxLength={255}
-                    isShort
-                  />
-                  <TextInput
-                    name="samplerNamer"
-                    labelText="Sampler Name"
-                    maxLength={255}
-                    isShort
-                  />
-                </FormSection>
-                <br />
-                <FormSection isOneLine4Elements>
-                  <PositionInput
-                    name="latitude"
-                    type="latitude"
-                    labelText="Lat"
-                    isShort
-                    autofill={!initialValues || refreshLatLong !== 0}
-                    isDisabled={isViewOnly}
-                    position={position?.latitude}
-                    isRequired
-                  />
-
-                  <div style={{ display: "flex" }}>
-                    <PositionInput
-                      name="longitude"
-                      type="longitude"
-                      labelText="Long"
-                      isShort
-                      autofill={!initialValues || refreshLatLong !== 0}
-                      isDisabled={isViewOnly}
-                      position={position?.longitude}
-                      isRequired
-                    />
-                    {!isViewOnly && (
-                      <Refresh
-                        isLoading={isLoading}
-                        setIsLoading={setIsLoading}
-                        setRefreshLatLong={setRefreshLatLong}
-                        refreshLatLong={refreshLatLong}
-                        testId="Refresh"
-                      />
-                    )}
-                  </div>
-                  <TextInput
-                    name="gpsMark"
-                    labelText="GPS Mark"
-                    maxLength={10}
-                    isShort
-                    isDisabled={isViewOnly}
-                  />
-                </FormSection>
+               
+            
               </section>
               <div css={utilities.sticky.footerContainer}>
                 <Link to={generateOpenEncounterURL(encounterId)}>
@@ -162,7 +62,7 @@ const BiopsyForm = ({
                   </Button>
                 </Link>
                 <Button type="submit" testId={"saveBiopsy"}>
-                  {!!initialValues ? "Save" : "Save"}
+                  Save
                 </Button>
               </div>
             </Form>
--- app/src/components/__tests__/BiopsyForm.test.js
+++ app/src/components/__tests__/BiopsyForm.test.js
@@ -1,93 +1,33 @@
 import React from "react";
-import { act, render } from "@testing-library/react";
+import { act, render, waitFor } from "@testing-library/react";
 import BiopsyForm from "../BiopsyForm";
-import { it } from "date-fns/locale";
+
+import userEvent from "@testing-library/user-event";
+
 
 describe.skip("BiopsyForm", () => {
-  beforeAll(() => {
-    global.Date.now = jest.fn(() =>
-      new Date("2020-05-04T11:30:12.000Z").getTime()
-    );
+  it("submits the form with correct values if all required fields are completed", async () => {
+    let formValues;
+    const mockHandleSubmit = (values) => {
+      formValues = values;
+    };
+   
+    const { getByRole } = render(
+      <BiopsyForm
+        handleSubmit={mockHandleSubmit}
+      />
+     );
+    
+     const speciesInput = getByRole("combobox", { name: "Species *" });
+     const submitButton = getByRole("button", { name: "Save" });
+
+     userEvent.selectOptions(speciesInput, "Fin whale");
+     userEvent.click(submitButton);
+
+      await waitFor(() => {
+      expect(formValues.species).toEqual("Fin whale");
+      })
   });
-
-  // it("submits the form with correct values if all fields are completed", async () => {
-  //   const realGeolocation = global.navigator.geolocation;
-
-  //   global.navigator.geolocation = {
-  //     getCurrentPosition: jest.fn().mockImplementation((success) =>
-  //       success({
-  //         coords: {
-  //           latitude: 1.123456,
-  //           longitude: 1.123456,
-  //         },
-  //       })
-  //     ),
-  //   };
-
-  //   let formValues;
-  //   const mockHandleSubmit = (values) => {
-  //     formValues = values;
-  //   };
-
-  //   const handleSubmit = jest.fn()
-  //   const { getByRole } = render(
-  //     <BiopsyForm handleSubmit={mockHandleSubmit} />
-  //   );
-
-  //   await act(async () => {
-  //     const speciesInput = getByRole("combobox", { name: "Species *" });
-  //     const latitudeInput = getByRole("spinbutton", { name: "Lat" });
-  //     const longitudeInput = getByRole("spinbutton", { name: "Long" });
-  //     const samplerNameInput = getByRole("textbox", { name: "Sampler Name" });
-  //     const attemptInput = getByRole("textbox", { name: "Attempt" });
-  //     const gpsMarkInput = getByRole("textbox", { name: "GPS Mark" });
-  //     const totalSpecimensInput = getByRole("textbox", {
-  //       name: "Total specimens",
-  //     });
-
-  //     const submitButton = getByRole("button", { name: "Save" });
-
-  //     userEvent.selectOptions(speciesInput, "Fin whale");
-  //     userEvent.clear(latitudeInput);
-  //     await userEvent.type(latitudeInput, "15.123456");
-  //     userEvent.clear(longitudeInput);
-  //     await userEvent.type(longitudeInput, "10.101010");
-  //     await userEvent.type(attemptInput, "1");
-  //     await userEvent.type(samplerNameInput, "T.U.");
-  //     await userEvent.type(gpsMarkInput, "123");
-  //     await userEvent.type(totalSpecimensInput, "5");
-  //     userEvent.click(submitButton);
-  //   });
-
-  //   expect(formValues.species).toEqual("Fin whale");
-  //   expect(formValues.latitude).toEqual("15.123456");
-  //   expect(formValues.longitude).toEqual("10.101010");
-  //   expect(formValues.gpsMark).toEqual("123");
-  //   expect(formValues.startTime).toEqual("11:30:12");
-  //   expect(formValues.longitude).toEqual("10.101010");
-  //   expect(formValues.startTime).toEqual("11:30:12");
-  //   expect(formValues.Date).toEqual("2020-05-04");
-  //   expect(formValues.endTime).toEqual("");
-  //   expect(formValues.attempt).toEqual("1");
-  //   expect(formValues.samplerName).toEqual("T.U.");
-
-  //   global.navigator.geolocation = realGeolocation;
-  // });
-
-  it.skip('rendering and submitting a basic Formik form', async () => {
-    const handleSubmit = jest.fn()
-    render(<BiopsyForm handleSubmit={handleSubmit} />)
-    const user = userEvent.setup()
-  
-    await act(async () => {
-     user.type(screen.getByRole("textbox", { name: "Sampler Name" }), "R");
-    },
-    
-    
-    expect(handleSubmit).toHaveBeenCalledWith({
-        samplerName: 'R'
-    }))
-  })
 
   it("contains cancel button", () => {
     const { queryByRole } = render(<BiopsyForm />);
