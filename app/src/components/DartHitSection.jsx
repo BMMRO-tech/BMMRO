e972143d0377e452fab6d1eac8cb9eac4b6b9001
JamesGoodThoughtworks <james.goodman@thoughtworks.com>
2022-07-06 13:44:24 +0100

    [#300] JG/ZG - Added svg to the document and made the test pass

--- app/src/components/DartHitSection.jsx
+++ app/src/components/DartHitSection.jsx
@@ -1,8 +1,21 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
+import { useState } from "react";
+import WhaleSvg from "./WhaleSvg";
 
 const DartHitSection = () => {
-  return <div></div>;
+  const [selectedSection, setSelectedSection] = useState("");
+
+  return (
+    <div>
+      <h3>Select Dart Hit Area</h3>
+      <WhaleSvg
+        setSelectedSection={setSelectedSection}
+        selectedSection={selectedSection}
+      />
+      <p>{selectedSection}</p>
+    </div>
+  );
 };
 
 export default DartHitSection;
