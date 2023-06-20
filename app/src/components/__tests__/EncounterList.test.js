4754cc1833ffa593f9dbfa8ed57c166214472458
rameezshafsad <rameez.shafsad@thoughtworks.com>
2023-06-20 16:32:33 +0100

    Merge with master

--- app/src/components/__tests__/EncounterList.test.js
+++ app/src/components/__tests__/EncounterList.test.js
@@ -1,5 +1,11 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
+import {
+  getByLabelText,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import EncounterList from "../EncounterList";
