76c4f9944270bdc465ddb87f5c2dc85f4a641784
rameezshafsad <rameez.shafsad@thoughtworks.com>
2023-07-10 14:33:05 +0100

    [#428] PC/RS/VD Add tests for readDocsByCollection if collection does not exist

---
+++ app/src/hooks/useProjectsFromFirestore.js
@@ -0,0 +1,25 @@
+import { CollectionNames } from "../datastore";
+
+const projectDb = async (datastore) => {
+  const extractProjectProperties = (project) => {
+    const { projectName } = project.data;
+    return projectName;
+  };
+
+  const projects = await datastore.readDocsFromCollection(
+    CollectionNames.PROJECT
+  );
+
+  console.log(projects);
+
+  if (!projects.length) {
+    return [{ entries: [] }];
+  }
+
+  const extractedProjects = projects.map((project) =>
+    extractProjectProperties(project)
+  );
+  return extractedProjects;
+};
+
+export default projectDb;
