4766c4e09391e9240095043ae73859a4eac21c34
Vlada <vladimir.davidovic@thoughtworks.com>
2023-07-31 17:36:29 +0100

    [#376] WIP - Import data from AccessDB to firebase
    Upload scripts - inserted a dummy JSON into Firestore DB

--- upload-script/src/__tests__/uploadCollectionData.test.js
+++ upload-script/src/__tests__/uploadCollectionData.test.js
@@ -10,14 +10,28 @@ describe("upload a collection to Firestore", function () {
                 area: "Soho, Central London",
                 biopsyAttempt: "No",
                 sequenceNumber: "E13"
-            }
+            },
+            {
+                encounterNumber: "05060708",
+                date: "13-May-2023",
+                startTime: "05:06",
+                area: "Barbican, East-ish London",
+                biopsyAttempt: "Thwarted",
+                sequenceNumber: "E24"
+            },
         ];
         uploadCollectionData(path, exportedJson);
         // expect(json).toEqual(expectedJson);
     });
 });
 
-
-
-
-
+    it("should upload a valid JSON to Firestore", function () {
+        const path = process.cwd();
+        const buffer = fs.readFileSync(path + "/src/__tests__/Encounter_test.json");
+        const data = buffer.toString();
+        console.log(data);
+        const collection  = "/encounter";
+        const exportedJson = JSON.parse(data);
+        uploadCollectionData(collection, exportedJson);
+    });
+});
