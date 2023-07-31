0ab0b1bb030a18ff26a0517cb7dd351db854c74f
rameezshafsad <rameez.shafsad@thoughtworks.com>
2023-07-31 14:12:39 +0100

    [#476] Fix jest issue

--- upload-script/src/uploadCollectionData.js
+++ upload-script/src/uploadCollectionData.js
@@ -1,25 +1,22 @@
-import {cert, initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
-
-import serviceAccount from "./auth/serviceAccountKey.json" assert {type: "json"}, deprecatedAssertSyntax: true;
-// import collection from "./__tests__/Encounter.json";
+import { cert, initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import serviceAccount from "./../auth/serviceAccountKey.json" assert { type: "json" };
 
 initializeApp({
-    credential: cert(serviceAccount),
+  credential: cert(serviceAccount),
 });
 
 const db = getFirestore();
 const batch = db.batch();
 
 function uploadCollectionData(collectionPath, collection) {
+  collection.forEach((proj) => {
+    const docRef = db.collection(collectionPath).doc();
+    batch.set(docRef, { collectionJson: collection });
+  });
 
-    collection.forEach((proj) => {
-        const docRef = db.collection(collectionPath).doc();
-        batch.set(docRef, {collectionJson: collection});
-    });
-
-    batch.commit();
-    return;
+  batch.commit();
+  return;
 }
 
-export {uploadCollectionData};
+export { uploadCollectionData };
