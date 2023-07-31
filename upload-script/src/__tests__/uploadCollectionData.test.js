2ac9b7294ad48ac5cde243526d06da13bb100b14
Paige Cox <paige.cox@thoughtworks.com>
2023-07-31 17:21:46 +0100

    [#476] PC/RS figured out how to upload dates correctly

--- upload-script/src/__tests__/uploadCollectionData.test.js
+++ upload-script/src/__tests__/uploadCollectionData.test.js
@@ -1,18 +1,16 @@
 import uploadCollectionData from "../uploadCollectionData.js";
-import * as fs from "fs";
 
 describe("upload a collection to Firestore", function () {
     it("should upload a dummy JSON to Firestore", function () {
         const path = "/encounter";
         const exportedJson = [
             {
-                encounterNumber: "99999999",
-                date: "20-May-2023",
-                startTime: "05:06",
+                encounterNumber: "10000000",
+                startTimestamp: new Date(1655722800),
                 area: "Soho, Central London",
                 biopsyAttempt: "No",
                 sequenceNumber: "E13"
-            },
+            }
         ];
         uploadCollectionData(path, exportedJson);
         // expect(json).toEqual(expectedJson);
--- upload-script/src/uploadScript.js
+++ upload-script/src/uploadScript.js
@@ -1,7 +1,7 @@
 import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-import serviceAccount from "./auth/serviceAccountKey.json" assert { type: "json" };
+import serviceAccount from "../auth/serviceAccountKey.json" assert { type: "json" };
 import project from "./project.js";
 
 initializeApp({
