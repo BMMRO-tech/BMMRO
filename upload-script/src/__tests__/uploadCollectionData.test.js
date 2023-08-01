07ed3da7480d0641684958e1a83d7ae56f6a9116
rameezshafsad <rameez.shafsad@thoughtworks.com>
2023-08-01 11:50:04 +0100

    [#476] PC/RS/VD upload subcollection to firestore

--- upload-script/src/__tests__/uploadCollectionData.test.js
+++ upload-script/src/__tests__/uploadCollectionData.test.js
@@ -2,35 +2,29 @@ import uploadCollectionData from "../uploadCollectionData.js";
 import * as fs from "fs";
 
 describe("upload a collection to Firestore", function () {
-    it("should upload a dummy JSON to Firestore", function () {
-        const path = "/encounter";
-        const exportedJson = [
-            {
-                encounterNumber: "10000000",
-                startTimestamp: new Date(1655722800),
-                area: "Soho, Central London",
-                biopsyAttempt: "No",
-                sequenceNumber: "E13"
-            },
-            {
-                encounterNumber: "05060708",
-                date: "13-May-2023",
-                startTime: "05:06",
-                area: "Barbican, East-ish London",
-                biopsyAttempt: "Thwarted",
-                sequenceNumber: "E24"
-            },
-        ];
-        uploadCollectionData(path, exportedJson);
-    });
+  it("should upload a dummy JSON to Firestore", function () {
+    const path = "/encounter";
+    const exportedJson = [
+      {
+        encounterNumber: "10000000",
+        startTimestamp: new Date(1655722800),
+        area: "Soho, Central London",
+        biopsyAttempt: "No",
+        sequenceNumber: "E13",
+      },
+      {
+        encounterNumber: "05060708",
+        date: "13-May-2023",
+        startTime: "05:06",
+        area: "Barbican, East-ish London",
+        biopsyAttempt: "Thwarted",
+        sequenceNumber: "E24",
+      },
+    ];
+    uploadCollectionData(path, exportedJson);
+  });
 
-    it("should upload a valid JSON to Firestore", function () {
-        const path = process.cwd();
-        const buffer = fs.readFileSync(path + "/src/__tests__/Encounter_test.json");
-        const data = buffer.toString();
-        console.log(data);
-        const collection = "/encounter";
-        const exportedJson = JSON.parse(data);
-        uploadCollectionData(collection, exportedJson);
-    });
+  it("should upload a valid JSON to Firestore", function () {
+    uploadCollectionData();
+  });
 });
--- upload-script/src/uploadCollectionData.js
+++ upload-script/src/uploadCollectionData.js
@@ -1,6 +1,7 @@
 import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import serviceAccount from "./../auth/serviceAccountKey.json" assert { type: "json" };
+import fs from "fs";
 
 initializeApp({
   credential: cert(serviceAccount),
@@ -8,16 +9,77 @@ initializeApp({
 
 const db = getFirestore();
 const batch = db.batch();
+const encounterPath = "/encounter";
+const habitatPath = "/habitatUse";
 
-function uploadCollectionData(collectionPath, collection) {
-
-  collection.forEach((record) => {
-    const docRef = db.collection(collectionPath).doc();
-    batch.set(docRef, record );
+function uploadData(
+  encounterJson,
+  habitatJson,
+  biopsyJson,
+  encounterSpecimenJson
+) {
+  encounterJson.forEach((record) => {
+    const docRef = db.collection(encounterPath).doc();
+    let encounterTimestamp = record.startTimestamp;
+    record.startTimestamp = new Date(record.startTimestamp * 1000);
+    batch.set(docRef, record);
+    habitatJson.forEach((habitat) => {
+      if (
+        encounterTimestamp === habitat.date &&
+        record.area === habitat.area &&
+        record.sequenceNumber === habitat.encSeqNo
+      ) {
+        delete habitat["date"];
+        docRef.collection("habitatUse").doc().set(habitat);
+      }
+    });
+    biopsyJson.forEach((biopsy) => {
+      if (
+        encounterTimestamp === biopsy.dateTaken &&
+        record.area === biopsy.area &&
+        record.sequenceNumber === biopsy.encSeqNo
+      ) {
+        biopsy.dateTaken = new Date(biopsy.dateTaken * 1000);
+        biopsy.species = record.species;
+        let biopsyDocRef = docRef.collection("biopsy").doc();
+        biopsyDocRef.set(biopsy);
+        encounterSpecimenJson.forEach((specimen) => {
+          if (
+            encounterTimestamp === specimen.dateTaken &&
+            record.area === specimen.area &&
+            record.sequenceNumber === specimen.encSeqNo
+          ) {
+            specimen.dateTaken = new Date(specimen.dateTaken * 1000);
+            biopsyDocRef.collection("specimen").doc().set(specimen);
+          }
+        });
+      }
+    });
   });
 
   batch.commit();
   return;
 }
 
-export default uploadCollectionData ;
+function uploadCollectionData() {
+  const path = process.cwd();
+  const encounterBuffer = fs.readFileSync(
+    path + "/src/__tests__/data/Encounter_test.json"
+  );
+  const habitatData = fs.readFileSync(
+    path + "/src/__tests__/data/habitat_test.json"
+  );
+  const biopsy = fs.readFileSync(path + "/src/__tests__/data/biopsy_test.json");
+
+  const specimen = fs.readFileSync(
+    path + "/src/__tests__/data/specimen_test.json"
+  );
+  const encounterJson = JSON.parse(encounterBuffer.toString());
+  const habitatJson = JSON.parse(habitatData.toString());
+  const biopsyJson = JSON.parse(biopsy.toString());
+  const encounterSpecimen = JSON.parse(specimen.toString());
+
+  uploadData(encounterJson, habitatJson, biopsyJson, encounterSpecimen);
+}
+
+export default uploadCollectionData;
