3ce4eb5d5c765700e5d788465e58db6b8d159f76
Jason Onwubiko <jason.g.onwubiko@accenture.com>
2026-07-17 14:51:21 +0100

    unit tests wip

--- export-script/src/__tests__/writeDataToFile.test.js
+++ export-script/src/__tests__/writeDataToFile.test.js
@@ -1,14 +1,15 @@
-jest.mock("fs");
-jest.mock("../helpers/logAndExit");
-jest.mock("../helpers/logToStdErrAndExit");
+vi.mock("fs");
+vi.mock("../helpers/logAndExit");
+vi.mock("../helpers/logToStdErrAndExit");
 const fs = require("fs");
 const writeDataToFile = require("../writeDataToFile");
 const logAndExit = require("../helpers/logAndExit");
 const logToStdErrAndExit = require("../helpers/logToStdErrAndExit");
+const { vi } = require("vitest");
 
 describe("writeDataToFile", () => {
   beforeAll(() => {
-    fs.existsSync = jest.fn(() => true);
+    fs.existsSync = vi.fn(() => true);
   });
 
   it("writes data to file", () => {
@@ -18,7 +19,7 @@ describe("writeDataToFile", () => {
   });
 
   it("exits the script if writing to file fails", () => {
-    fs.writeFileSync = jest.fn(() => {
+    fs.writeFileSync = vi.fn(() => {
       throw new Error("🤬");
     });
     writeDataToFile("dummyDir", "dummyFile", "test");
